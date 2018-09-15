6caf9b0f8d80f1acce1110027d40bf01db6cc85e
Marco Hartmann <marcohartmann95@gmail.com>
2018-09-15 02:05:47 +0200

    Add of matching

--- uart_drv.c
+++ uart_drv.c
@@ -9,20 +9,28 @@
 static int serial_probe(struct platform_device *pdev)
 {
 	pr_info("Called serial_probe\n");
+
 	return 0;
 }
 
 static int serial_remove(struct platform_device *pdev)
 {
 	pr_info("Called serial_remove\n");
-        return 0;
+
+    return 0;
 }
 
 /****************************** driver structures *****************************/
+static const struct of_device_id uart_match[] = {
+	{ .compatible = "mh,uart_dev" },
+	{ /* sentinel */ },
+};
+
 static struct platform_driver serial_driver = {
         .driver = {
                 .name = "UART driver",
                 .owner = THIS_MODULE,
+                .of_match_table = uart_match,
         },
         .probe = serial_probe,
         .remove = serial_remove,
